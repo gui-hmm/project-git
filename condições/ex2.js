d14e61d34eb1cf6ec0402ac5624943f247de0ba3
gui-hmm <henriquemelomoura@gmail.com>
2023-08-15 10:15:38 -0300

    first commit

---
+++ condições/ex2.js
@@ -0,0 +1,8 @@
+var país = "Espanha"
+console.log(`To vivendo no ${país}`)
+
+if(país == 'Brasil'){
+    console.log('voce é brasileiro')
+}else{
+    console.log('você é estrangeiro')
+}
---
+++ condições/index.js
@@ -0,0 +1,8 @@
+var vel = 78.2
+
+
+console.log(`a velocidade do seu carro é ${vel} km/h`)
+if(vel>60){ //condição simples!
+       console.log("voce ultrapassou a velocidade e sera multado")
+}
+console.log(`Dirija sempre usando o cinto de segurança`)
